90dcf44f3fb8e28568627df7b1b0f10d86b4480a
DongYoung Lim <dz1120@gmail.com>
2019-04-08 02:35:03 +0000

    First Commit

--- instagram/settings.py
+++ instagram/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't*bbt-))hhvvgox(yf1t0jg-kh6g_7dm#&kc7rdpz8haxd@!l4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['last-pang-egyeasy.c9users.io']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
